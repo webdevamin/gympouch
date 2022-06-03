cc522c06ff7d303600fbadfdaa53414e850424af
1chbinamin <amin.m.intichev@gmail.com>
2022-06-03 16:06:22 +0200

    finish proto dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      light: "#E9E9F3",
+      dark: "#1E1E20",
+      green: "#0ebb00",
+      light_green: "#56ff48",
+    },
+  },
+  plugins: [],
+};
