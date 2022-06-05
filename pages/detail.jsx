e2254ecee73ad80621f147ab8ee627391642f1e5
1chbinamin <amin.m.intichev@gmail.com>
2022-06-05 02:57:59 +0200

    detail page

---
+++ pages/detail.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Header from '../components/Header';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Detail = () => {
+    return (
+        <>
+            <Header title={"Liesbeth Roels"} />
+            <main className='relative'>
+                <section className='rounded-2xl'>
+                    <Image className='rounded-2xl' src="/map.png"
+                        height={370} width={370} responsive alt={'Map'} />
+                </section>
+                <section className='mt-6 text-center flex flex-col 
+                items-center justify-center'>
+                    <div>
+                        <h2 className='font-extrabold text-2xl'>Liesbeth Roels</h2>
+                        <p className='text-gray font-medium'>Stronglifts (B) + Bur</p>
+                    </div>
+                    <div className='mt-2 flex gap-6 items-center justify-center'>
+                        <div className='flex items-center gap-1.5'>
+                            <Image src="/activity-red.svg"
+                                height={18} width={18} alt={'Conditie'} />
+                            <span className='text-gray'>Conditie</span>
+                        </div>
+                        <div className='flex items-center gap-1.5'>
+                            <Image src="/map-pin-red.svg"
+                                height={18} width={18} alt={'Locatie'} />
+                            <span className='text-gray'>BB, Jabbeke</span>
+                        </div>
+                    </div>
+                </section>
+                <section className='flex gap-3 mt-5'>
+                    <article className='flex-1 bg-white p-6 rounded-2xl flex flex-col 
+                    justify-center items-center gap-4 lighter_neu'>
+                        <div className='bg-red inline-flex py-2 px-4 rounded-md w-fit'>
+                            <Image src="/clock-white.svg"
+                                height={20} width={20} alt={'Tijd'} />
+                        </div>
+                        <div className='text-center'>
+                            <h3 className='font-extrabold pb-1'>10:15</h3>
+                            <p className='text-gray'>1u - 1u30</p>
+                        </div>
+                    </article>
+                    <article className='flex-1 bg-white p-6 rounded-2xl flex flex-col 
+                    justify-center items-center gap-4 lighter_neu'>
+                        <div className='bg-red inline-flex py-2 px-4 rounded-md w-fit'>
+                            <Image src="/dollar-sign-white.svg"
+                                height={20} width={20} alt={'Kost'} />
+                        </div>
+                        <div className='text-center'>
+                            <h3 className='font-extrabold pb-1'>€ 60.00</h3>
+                            <p className='text-gray'>Totaal</p>
+                        </div>
+                    </article>
+                </section>
+                <section className='flex mt-14 justify-between items-center'>
+                    <div>
+                        <Link href={'/'}>
+                            <a className='font-bold text-lg flash_dark 
+                            text-white px-16 py-3 rounded-2xl tr'>Terug</a>
+                        </Link>
+                    </div>
+                    <div>
+                        <button className='px-16 py-3 flash_red rounded-2xl 
+                        text-white font-bold'>Finish</button>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default Detail
--- pages/index.jsx
+++ pages/index.jsx
@@ -43,53 +43,61 @@ const Index = () => {
           </div>
 
           <div className='cards'>
-            <article className='card'>
-              <div className='flex justify-between items-baseline'>
-                <div>
-                  <div className='circle_text bg-pink w-8 h-8'>LR</div>
-                  <span className='text-xs font-semibold text-lightdark'>Liesbeth</span>
-                </div>
-                <div className='circle_text w-5 h-5'>
-                  <Image src="/activity.svg" height={15} width={15} alt={'Conditie'} />
-                </div>
-              </div>
-              <div className='mt-2'>
-                <h3 className='card_title'>Stronglifts (B) + Bur</h3>
-                <div className='flex items-end gap-2'>
-                  <span className='text-xs flex items-center gap-1'>
-                    <Image src="/clock.svg" height={15} width={15} alt={'Tijd'} />
-                    <span className='font-semibold'>1u - 1u30</span>
-                  </span>
-                  <span className='text-xs text-lightdark font-medium'>
-                    BB, Jabbeke
-                  </span>
-                </div>
-              </div>
-            </article>
+            <Link href={'/detail'}>
+              <a>
+                <article className='card'>
+                  <div className='flex justify-between items-baseline'>
+                    <div>
+                      <div className='circle_text bg-pink w-8 h-8'>LR</div>
+                      <span className='text-xs font-semibold text-lightdark'>Liesbeth</span>
+                    </div>
+                    <div className='circle_text w-5 h-5'>
+                      <Image src="/activity.svg" height={15} width={15} alt={'Conditie'} />
+                    </div>
+                  </div>
+                  <div className='mt-2'>
+                    <h3 className='card_title'>Stronglifts (B) + Bur</h3>
+                    <div className='flex items-end gap-2'>
+                      <span className='text-xs flex items-center gap-1'>
+                        <Image src="/clock.svg" height={15} width={15} alt={'Tijd'} />
+                        <span className='font-semibold'>1u - 1u30</span>
+                      </span>
+                      <span className='text-xs text-lightdark font-medium'>
+                        BB, Jabbeke
+                      </span>
+                    </div>
+                  </div>
+                </article>
+              </a>
+            </Link>
 
-            <article className='card'>
-              <div className='flex justify-between items-baseline'>
-                <div>
-                  <div className='circle_text bg-purple w-8 h-8'>GV</div>
-                  <span className='text-xs font-semibold text-lightdark'>Guido</span>
-                </div>
-                <div>
-                  <Image src="/life-buoy.svg" height={15} width={15} alt={'Core'} />
-                </div>
-              </div>
-              <div className='mt-2'>
-                <h3 className='card_title'>RFT (- Core)</h3>
-                <div className='flex items-end gap-2'>
-                  <span className='text-xs flex items-center gap-1'>
-                    <Image src="/clock.svg" height={15} width={15} alt={'Tijd'} />
-                    <span className='font-semibold'>&gt; 1u</span>
-                  </span>
-                  <span className='text-xs text-lightdark font-medium'>
-                    BB, Jabbeke
-                  </span>
-                </div>
-              </div>
-            </article>
+            <Link href={'/detail'}>
+              <a>
+                <article className='card'>
+                  <div className='flex justify-between items-baseline'>
+                    <div>
+                      <div className='circle_text bg-purple w-8 h-8'>GV</div>
+                      <span className='text-xs font-semibold text-lightdark'>Guido</span>
+                    </div>
+                    <div>
+                      <Image src="/life-buoy.svg" height={15} width={15} alt={'Core'} />
+                    </div>
+                  </div>
+                  <div className='mt-2'>
+                    <h3 className='card_title'>RFT (- Core)</h3>
+                    <div className='flex items-end gap-2'>
+                      <span className='text-xs flex items-center gap-1'>
+                        <Image src="/clock.svg" height={15} width={15} alt={'Tijd'} />
+                        <span className='font-semibold'>&gt; 1u</span>
+                      </span>
+                      <span className='text-xs text-lightdark font-medium'>
+                        BB, Jabbeke
+                      </span>
+                    </div>
+                  </div>
+                </article>
+              </a>
+            </Link>
           </div>
         </section>
         <section>
@@ -98,56 +106,68 @@ const Index = () => {
             <span className='font-medium text-lightdark text-sm'>Bekijk meer</span>
           </div>
           <div className="list">
-            <article className="list_item">
-              <div className='flex items-center gap-3'>
-                <div>
-                  <div className='circle_text bg-matt_green w-10 h-10'>MV</div>
-                </div>
-                <div>
-                  <h3 className='font-semibold'>Max Vangampelaere</h3>
-                  <p className='text-sm text-lightdark'>3RFT WOD, nieuw set en core</p>
-                </div>
-              </div>
-              <div className='flex flex-col justify-center items-end'>
-                <small className='text-lightdark font-medium'>14:30</small>
-                <small className='bg-red w-5 h-5 flex justify-center items-center 
+            <Link href={'/detail'}>
+              <a>
+                <article className="list_item">
+                  <div className='flex items-center gap-3'>
+                    <div>
+                      <div className='circle_text bg-matt_green w-10 h-10'>MV</div>
+                    </div>
+                    <div>
+                      <h3 className='font-semibold'>Max Vangampelaere</h3>
+                      <p className='text-sm text-lightdark'>3RFT WOD, nieuw set en core</p>
+                    </div>
+                  </div>
+                  <div className='flex flex-col justify-center items-end'>
+                    <small className='text-lightdark font-medium'>14:30</small>
+                    <small className='bg-red w-5 h-5 flex justify-center items-center 
                 rounded-full text-white text-xs font-medium mt-1.5'>2</small>
-              </div>
-            </article>
-            <article className="list_item">
-              <div className='flex items-center gap-3'>
-                <div>
-                  <div className='circle_text bg-deep_blue w-10 h-10'>AE</div>
-                </div>
-                <div>
-                  <h3 className='font-semibold'>Aziz Eryilmaz</h3>
-                  <p className='text-sm text-lightdark'>Gunny, skip</p>
-                </div>
-              </div>
-              <div>
-                <small className='text-lightdark font-medium'>16:00</small>
-              </div>
-            </article>
-            <article className="list_item">
-              <div className='flex items-center gap-3'>
-                <div>
-                  <div className='circle_text bg-red w-10 h-10'>MV</div>
-                </div>
-                <div>
-                  <h3 className='font-semibold'>Brecht Demey</h3>
-                  <p className='text-sm text-lightdark'>3RFT WOD</p>
-                </div>
-              </div>
-              <div>
-                <small className='text-lightdark font-medium'>19:30</small>
-              </div>
-            </article>
+                  </div>
+                </article>
+              </a>
+            </Link>
+            <Link href={'/detail'}>
+              <a>
+                <article className="list_item">
+                  <div className='flex items-center gap-3'>
+                    <div>
+                      <div className='circle_text bg-deep_blue w-10 h-10'>AE</div>
+                    </div>
+                    <div>
+                      <h3 className='font-semibold'>Aziz Eryilmaz</h3>
+                      <p className='text-sm text-lightdark'>Gunny, skip</p>
+                    </div>
+                  </div>
+                  <div>
+                    <small className='text-lightdark font-medium'>16:00</small>
+                  </div>
+                </article>
+              </a>
+            </Link>
+            <Link href={'/detail'}>
+              <a>
+                <article className="list_item">
+                  <div className='flex items-center gap-3'>
+                    <div>
+                      <div className='circle_text bg-red w-10 h-10'>MV</div>
+                    </div>
+                    <div>
+                      <h3 className='font-semibold'>Brecht Demey</h3>
+                      <p className='text-sm text-lightdark'>3RFT WOD</p>
+                    </div>
+                  </div>
+                  <div>
+                    <small className='text-lightdark font-medium'>19:30</small>
+                  </div>
+                </article>
+              </a>
+            </Link>
           </div>
         </section>
 
         <section className='footer'>
           <div className='footer_child'>
-            <Link href={'/'}>
+            <Link href={'/detail'}>
               <a className='new_btn'>
                 <Image src="/plus.svg" height={18} width={18} alt={'Zoeken'} />
                 <span className='text-white'>Nieuw afspraak</span>
